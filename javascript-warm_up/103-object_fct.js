74c9fd6fb714897441d76460bdf4ccb3a20dfc33
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2023-09-28 18:20:45 +0100

    Edit file ensured the console gave function not function (anonymous)

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -1,15 +1,18 @@
 #!/usr/bin/node
 const myObject = {
     type: 'object',
-    value: 12
+    value: 12,
+    incr: function(){
+      myObject.value++;
+    }
   };
   console.log(myObject);
 
 //   my code starts here
-myObject.incr = function() {
-  myObject.value++;
+// myObject.incr = function() {
+//   myObject.value++;
     
-  }
+//   }
 
 myObject.incr();
   console.log(myObject);
